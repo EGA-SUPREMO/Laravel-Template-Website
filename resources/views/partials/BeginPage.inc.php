99bbdc830fac2a6529a9e3669c6fa7226c44e8e6
EGA-SUPREMO <cxiesestro@gmail.com>
2020-06-04 09:15:59 -0400

    Change BeginPage
    Signed-off-by: EGA-SUPREMO <cxiesestro@gmail.com>

--- resources/views/partials/BeginPage.inc.php
+++ resources/views/partials/BeginPage.inc.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
-<html lang = "es">
+<html lang = "{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <title><?php echo $title; ?></title>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="robots" content="noindex">
+
+        <title><?php echo $title; ?></title>
+
+        <!-- Styles -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
         <link href="<?php echo DIR_CSS?>styles.css" rel="stylesheet">
-        <link href="<?php echo DIR_CSS?>fontawesome.min.css" rel="stylesheet">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
