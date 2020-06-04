a884a18e3740b6ce2797f993c24ce395d457807d
EGA-SUPREMO <cxiesestro@gmail.com>
2020-06-04 12:18:07 -0400

    Fix a few bugs in home view & add ui/laravel
    Signed-off-by: EGA-SUPREMO <cxiesestro@gmail.com>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,15 @@
 <?php
 
 include_once 'app/WriterEntries.inc.php';
-
-include_once 'templates/BeginPage.inc.php';
-include_once 'templates/Navbar.inc.php';
 ?>
+@include('partials.beginPage')
+@include('partials.navbar')
+
 <div class="container">
     <div class="jumbotron">
-        <h1>A test Blog</h1>
+        <h1>{{  }}</h1>
         <p>
-            The best test blog for show my skills
+            {{  }}
         </p>
     </div>
 </div>
@@ -24,7 +24,7 @@ include_once 'templates/Navbar.inc.php';
                         </div>
                         <div class="card-body">
                             <div class="form-group">
-                                <form class="form-inline" action="<?php echo URL_SEARCH; ?>" method='GET'>
+                                <form class="form-inline" action="{{ URL_SEARCH }}" method='GET'>
                                     <input class="form-control mr-sm-2" type="search" placeholder="Search ..." aria-label="Search" name="q">
                                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="submit">Search</button>
                                 </form>
@@ -36,13 +36,9 @@ include_once 'templates/Navbar.inc.php';
         </div>
         <div class="col-md-8">
             <?php
-            Conection::openConection();
             WriterEntries::writeEntry();
-            Conection::closeConection();
             ?>
         </div>
     </div>
 </div>
-<?php
-include_once 'templates/EndPage.inc.php';
-?>
+@include('partials.endPage')
