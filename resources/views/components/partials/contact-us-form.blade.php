47b62812c16cb95b5443f76d0b20386950047da2
EGA-SUPREMO <cxiesestro@gmail.com>
2020-06-20 14:04:17 -0400

    add small label below each input & error messages

--- resources/views/components/partials/contact-us-form.blade.php
+++ resources/views/components/partials/contact-us-form.blade.php
@@ -8,15 +8,30 @@
         		@csrf
 				<div class="form-group">
 					<label for="name">Nombre</label>
-					<input type="text" class="form-control" id="name" name="name" required placeholder="Por favor escribe tu nombre">
-				</div>
+					<input type="text" class="form-control" id="name" name="name" aria-describedby="name-help" required placeholder="Por favor escribe tu nombre">
+                    <small id="name-help" class="form-text text-muted">Para saber quien eres</small>
+                    
+                    @error('name')
+                        <small class="text-danger">{{ $message }}</small>
+                    @enderror
+                </div>
 				<div class="form-group">
 					<label for="email">Email</label>
-					<input type="email" class="form-control" id="email" name="email" required placeholder="Por favor escribe tu email">
+					<input type="email" class="form-control" id="email" name="email" aria-describedby="email-help" required placeholder="Por favor escribe tu email">
+					<small id="email-help" class="form-text text-muted">As`i podremos responderte</small>
+                    
+                    @error('email')
+                        <small class="text-danger">{{ $message }}</small>
+                    @enderror
 				</div>
 				<div class="form-group">
 					<label for="body">Mensaje</label>
-					<textarea class="form-control" rows="5" id="body" name="body" placeholder="Escribe tu mensaje"></textarea>
+					<textarea class="form-control" rows="5" id="body" name="body" aria-describedby="body-help" required placeholder="Escribe tu mensaje"></textarea>
+					<small id="body-help" class="form-text text-muted">Describe que necesitas</small>
+                    
+                    @error('body')
+                        <small class="text-danger">{{ $message }}</small>
+                    @enderror
 				</div>
 				<br>
 				<button type="submit" class="btn btn-primary" name="save">Enviar</button>
