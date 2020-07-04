f9b944083d8e8f9d5fe0e054200b6b9f6f307e01
EGA-SUPREMO <cxiesestro@gmail.com>
2020-07-04 17:00:17 -0400

    update names in lang files

--- resources/lang/en/welcome.php
+++ resources/lang/en/welcome.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-	'Small description' => 'Dummy text',
-	'Dummy card-header' => 'Dummy header'
-	'Dummy card-items' => [
+	'small description' => 'Dummy text',
+	'card-header' => 'Dummy header'
+	'card-items' => [
 		'Dummy item 1',
 		'Dummy item 2',
 		'Dummy item 3',
--- resources/lang/es/welcome.php
+++ resources/lang/es/welcome.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-	'Small description' => 'Dummy text',
-	'Dummy card-header' => 'Servicios que ofrecemos'
-	'Dummy card-items' => [
+	'small description' => 'Dummy text',
+	'card-header' => 'Servicios que ofrecemos'
+	'card-items' => [
 		'Asesoramiento financiero',
 		'Asesoramiento en criptoactivos',
 		'Protección contra la inflación',
