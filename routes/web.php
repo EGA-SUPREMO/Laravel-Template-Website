01b29736eb07af1c04a3c2c0b8abcd28ef920c6b
EGA-SUPREMO <cxiesestro@gmail.com>
2020-07-04 17:13:37 -0400

    add route for the spanish version

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/es', function () {
+	App::setLocale('es');
+
+    return view('welcome');
+});
