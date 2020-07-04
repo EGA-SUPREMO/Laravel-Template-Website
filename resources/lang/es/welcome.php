c44ca8153c0d8f2c9ccb12fd5d8a3c1711875330
EGA-SUPREMO <cxiesestro@gmail.com>
2020-07-04 16:57:00 -0400

    add es welcome lang file

---
+++ resources/lang/es/welcome.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	'Small description' => 'Dummy text',
+	'Dummy card-header' => 'Servicios que ofrecemos'
+	'Dummy card-items' => [
+		'Asesoramiento financiero',
+		'Asesoramiento en criptoactivos',
+		'Protección contra la inflación',
+		'Dummy item 3',
+	],
+];
