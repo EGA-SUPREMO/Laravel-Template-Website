f59a74cae7314c879037a51fd1d3c30c07993ee6
Alejandro Garcia <cxiesestro@gmail.com>
2020-07-06 21:07:17 -0400

    Translate contact-us form (#18)
    * extract text
    * finish spanish translation & contact partial config
    * add en translation

---
+++ resources/lang/es/contact-form.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+	'card-header' => 'Contactanos',
+	'name' => 'Nombre',
+	'name-helper' => 'Para saber quien eres.',
+	'name-placeholder' => 'Por favor escribe tu nombre',
+	'email' => 'Email',
+	'email-helper' => 'Así podremos responderte.',
+	'email-placeholder' => 'Por favor escribe tu email',
+	'body' => 'Mensaje',
+	'body-helper' => 'Describe que necesitas.',
+	'body-placeholder' => 'Escribe tu mensaje',
+	'send-button' => 'Enviar',
+];
--- resources/views/components/partials/contact-us-form.blade.php
+++ resources/views/components/partials/contact-us-form.blade.php
@@ -1,40 +1,40 @@
 <div class="col-md-12">
     <div class="card">
         <div class="card-header">
-            Contactanos
+            {{ __('contact-form.card-header') }}
         </div>
         <div class="card-body">
         	<form class="form-contact-us" method="post" action="#">
         		@csrf
 				<div class="form-group">
-					<label for="name">Nombre</label>
-					<input type="text" class="form-control" id="name" name="name" aria-describedby="name-help" required placeholder="Por favor escribe tu nombre">
-                    <small id="name-help" class="form-text text-muted">Para saber quien eres</small>
+					<label for="name">{{ __('contact-form.name') }}</label>
+					<input type="text" class="form-control" id="name" name="name" aria-describedby="name-help" required placeholder="{{ __('contact-form.name-placeholder') }}">
+                    <small id="name-help" class="form-text text-muted">{{ __('contact-form.name-helper') }}</small>
                     
                     @error('name')
                         <small class="text-danger">{{ $message }}</small>
                     @enderror
                 </div>
 				<div class="form-group">
-					<label for="email">Email</label>
-					<input type="email" class="form-control" id="email" name="email" aria-describedby="email-help" required placeholder="Por favor escribe tu email">
-					<small id="email-help" class="form-text text-muted">Así podremos responderte</small>
+					<label for="email">{{ __('contact-form.email') }}</label>
+					<input type="email" class="form-control" id="email" name="email" aria-describedby="email-help" required placeholder="{{ __('contact-form.email-placeholder') }}">
+					<small id="email-help" class="form-text text-muted">{{ __('contact-form.email-helper') }}</small>
                     
                     @error('email')
                         <small class="text-danger">{{ $message }}</small>
                     @enderror
 				</div>
 				<div class="form-group">
-					<label for="body">Mensaje</label>
-					<textarea class="form-control" rows="5" id="body" name="body" aria-describedby="body-help" required placeholder="Escribe tu mensaje"></textarea>
-					<small id="body-help" class="form-text text-muted">Describe que necesitas</small>
+					<label for="body">{{ __('contact-form.body') }}</label>
+					<textarea class="form-control" rows="5" id="body" name="body" aria-describedby="body-help" required placeholder="{{ __('contact-form.body-placeholder') }}"></textarea>
+					<small id="body-help" class="form-text text-muted">{{ __('contact-form.body-helper') }}</small>
                     
                     @error('body')
                         <small class="text-danger">{{ $message }}</small>
                     @enderror
 				</div>
 				<br>
-				<button type="submit" class="btn btn-primary" name="save">Enviar</button>
+				<button type="submit" class="btn btn-primary" name="save">{{ __('contact-form.send-button') }}</button>
             </form> 
         </div>
     </div>
