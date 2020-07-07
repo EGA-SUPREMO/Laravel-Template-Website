bf6fe25c5a736de6a01e0f6d69075e91c601c9aa
EGA-SUPREMO <cxiesestro@gmail.com>
2020-07-06 20:20:49 -0400

    add en translate

---
+++ resources/lang/en/contact-form.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+	'card-header' => 'Contact us',
+	'name' => 'Name',
+	'name-helper' => 'To know who you are.',
+	'name-placeholder' => 'Please put your name',
+	'email' => 'Email',
+	'email-helper' => 'This way we can know who to reply.',
+	'email-placeholder' => 'Please put your email',
+	'body' => 'Message',
+	'body-helper' => 'Describe what you need.',
+	'body-placeholder' => 'Write your message',
+	'send-button' => 'Send',
+];
