63177d681ffa8fee3a7704c0acea38a7acc7a956
EGA-SUPREMO <cxiesestro@gmail.com>
2020-07-07 09:35:59 -0400

    add navbar english translation

---
+++ resources/lang/en/navbar.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+	'home' => 'Home',
+	'service' => 'Services',
+	'service-items' => [
+		'Financial advice',
+		'Advice on crypto assets',
+		'Protection against inflation',
+	],
+	'about-us' => 'About Us',
+	'en' => 'English',
+	'es' => 'Spanish',
+];
