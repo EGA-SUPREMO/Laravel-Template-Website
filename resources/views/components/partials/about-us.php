5cf3f021f5f5a89d35475988edcc2beaa0dcd9d1
EGA-SUPREMO <cxiesestro@gmail.com>
2020-07-14 16:54:12 -0400

    add about-us component

---
+++ resources/views/components/partials/about-us.php
@@ -0,0 +1,10 @@
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header">
+            {{ __('about-us.card-header') }}
+        </div>
+        <div class="card-body">
+            @foreach(__('about-us.card-body')
+        </div>
+    </div>
+</div>
