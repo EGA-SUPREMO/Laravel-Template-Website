7f2c8fe42bdc4ec2cc6d096d005c94d75de736e8
Alejandro Garcia <cxiesestro@gmail.com>
2020-07-15 14:41:09 -0400

    Add about us section (#25)
    * add about-us component
    * add lang files

---
+++ resources/views/components/partials/about-us.blade.php
@@ -0,0 +1,12 @@
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header">
+            {{ __('about-us.card-header') }}
+        </div>
+        <div class="card-body">
+            {{ __('about-us.card-body') }}
+            <hr>
+            Whatsapp: +58 426 2464 450
+        </div>
+    </div>
+</div>
